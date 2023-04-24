e88d330707dbcf8230c0a6b5c163265560feb4dc
Thiago Brumer <thiago.brumer@hotmart.com>
2023-04-24 19:38:17 -0300

    starting nodejs app

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors');
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/app', express.static(path.join(__dirname, '/public')))
+
+app.listen(port)
