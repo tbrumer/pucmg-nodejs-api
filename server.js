af78fb944f0dde9caa896731bdcdcb7a5b4cd4c2
Thiago Brumer <thiago.brumer@hotmart.com>
2023-04-25 20:36:18 -0300

    trying to deploy app to webdock

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-require('dotenv').config()
+// require('dotenv').config()
 
-const express = require('express')
-const cors = require('cors');
-const path = require('path')
-const app = express()
-const port = process.env.PORT || 3000
+// const express = require('express')
+// const cors = require('cors');
+// const path = require('path')
+// const app = express()
+// const port = process.env.PORT || 3000
 
-app.use(cors())
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/app', express.static(path.join(__dirname, '/public')))
+// app.use(cors())
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use('/app', express.static(path.join(__dirname, '/public')))
 
-app.listen(port)
+// app.listen(port)
 
-// const express = require('express');
-// const https = require('https');
-// const fs = require('fs');
-// const options = {
-//     key: fs.readFileSync('/etc/letsencrypt/live/pucmg.vps.webdock.cloud/privkey.pem'),
-//     cert: fs.readFileSync('/etc/letsencrypt/live/pucmg.vps.webdock.cloud/fullchain.pem')
-// };
-// const app = express();
-// app.use((req, res) =>
-// {
-//     res.end('Hello World');
-// });
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/pucmg.vps.webdock.cloud/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/pucmg.vps.webdock.cloud/fullchain.pem')
+};
+const app = express();
+app.use((req, res) =>
+{
+    res.end('Hello World');
+});
 
-// https.createServer(options, app).listen(8000);
+https.createServer(options, app).listen(3000);
