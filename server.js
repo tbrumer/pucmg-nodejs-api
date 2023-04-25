50acadf986bc7df61de254d97ea513d52e881dc0
Thiago Brumer <thiago.brumer@hotmart.com>
2023-04-25 20:51:28 -0300

    trying to deploy app to webdock

--- server.js
+++ server.js
@@ -1,29 +1,24 @@
-// require('dotenv').config()
+require('dotenv').config()
 
-// const express = require('express')
-// const cors = require('cors');
-// const path = require('path')
-// const app = express()
-// const port = process.env.PORT || 3000
-
-// app.use(cors())
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use('/app', express.static(path.join(__dirname, '/public')))
-
-// app.listen(port)
-
-const express = require('express');
+const express = require('express')
 const https = require('https');
+const cors = require('cors');
+const path = require('path')
 const fs = require('fs');
+
 const options = {
     key: fs.readFileSync('/etc/letsencrypt/live/pucmg.vps.webdock.cloud/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/pucmg.vps.webdock.cloud/fullchain.pem')
 };
-const app = express();
-app.use((req, res) =>
-{
-    res.end('Hello World');
-});
 
-https.createServer(options, app).listen(3000);
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/app', express.static(path.join(__dirname, '/public')))
+
+// app.listen(port)
+https.createServer(options, app).listen(port);
