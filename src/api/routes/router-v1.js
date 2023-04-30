7125e7b1fc90345fe61ee15ebbeb4d55e0f4e2b5
Thiago Brumer <thiago.brumer@hotmart.com>
2023-04-30 13:25:39 -0300

    add crud enpoints

--- src/api/routes/router-v1.js
+++ src/api/routes/router-v1.js
@@ -2,26 +2,59 @@ import { Router } from 'express'
 
 import { produtos } from '../../database'
 
-let routerV1 = Router()
+const routerV1 = Router()
+const endpoint = '/v1/'
 
-const endpoint = '/'
+let id = Math.max(...produtos.map(p => p.id))
 
 routerV1.get(endpoint + 'produtos', function (_req, res) {
     res.status(200).json(produtos)
 })
 
 routerV1.get(endpoint + 'produtos/:id', function (req, res) {
-    res.status(200).json(produtos.find(({ id }) => id.toString() === req.params['id']))
+    res.status(200).json(getProductById(req.params.id))
 })
 
-routerV1.post(endpoint + 'produtos/', function (req, res) {
-    produtos.push({
-        id: produtos.length++,
-        descricao: req.body['descricao'],
-        valor: req.body['valor'],
-        marca: req.body['marca']
-    })
-    res.status(200).json("Produto adicionado com sucesso!")
+routerV1.post(endpoint + 'produtos', function (req, res) {
+    const product = {
+        id: ++id,
+        descricao: req.body.descricao,
+        valor: req.body.valor,
+        marca: req.body.marca
+    }
+    produtos.push(product)
+
+    res.status(201).json(product)
 })
+
+routerV1.put(endpoint + 'produtos/:id', function (req, res) {
+    const product = getProductById(req.params.id)
+
+    if (product) {
+        product.descricao = req.body.descricao
+        product.valor = req.body.valor
+        product.marca = req.body.marca
+    }
+
+    res.status(200).json(product)
+})
+
+routerV1.delete(endpoint + 'produtos/:id', function (req, res) {
+    const index = getProductIndexById(req.params.id)
+
+    if (index >= 0) {
+        produtos.splice(getProductIndexById(req.params.id), 1)
+    }
+
+    res.status(204).end()
+})
+
+function getProductById(productId) {
+    return produtos.find(({ id }) => id === parseInt(productId))
+}
+
+function getProductIndexById(productId) {
+    return produtos.findIndex(({ id }) => id === parseInt(productId))
+}
 
 export default routerV1;
